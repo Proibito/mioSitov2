e0ac3fed66d732f582891e0764f7a342f4dadd2c
Edoardo Balzano <edoardobalzano01@gmail.com>
2022-09-23 14:39:22 +0200

    sistematiche meccaninche del sito e aggiunta di appunti nuovi

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import image from "@astrojs/image";
 import react from "@astrojs/react";
-import { ottieniDescrizione, ottieniSimboli } from "./src/functions/ottieniDescrizione.ts";
+import {
+  ottieniDescrizione,
+  ottieniSimboli,
+} from "./src/functions/ottieniDescrizione.ts";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
