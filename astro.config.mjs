5fb857f3277cea2675624c84262b824bcf5be69d
Proibito04 <30660830+Proibito@users.noreply.github.com>
2022-09-25 17:29:25 +0200

    aggiunte nuove funzionalià e corrette altre cose

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,43 @@ import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import image from "@astrojs/image";
 import react from "@astrojs/react";
-import { ottieniDescrizione, ottieniSimboli } from "./src/functions/ottieniDescrizione.ts";
+import {
+  ottieniDescrizione,
+  ottieniSimboli,
+  boxDefinizione,
+  codiceInline,
+} from "./src/functions/pluginsRemark";
 import mdx from "@astrojs/mdx";
+import remarkDirective from "remark-directive";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: "min-light",
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+    },
+  },
   integrations: [
     image(),
     react(),
     mdx({
-      remarkPlugins: [ottieniDescrizione, remarkMath, ottieniSimboli],
+      remarkPlugins: [
+        ottieniDescrizione,
+        remarkMath,
+        ottieniSimboli,
+        remarkDirective,
+        ottieniSimboli,
+        boxDefinizione,
+        codiceInline,
+      ],
       rehypePlugins: [rehypeKatex],
       extendPlugins: "astroDefaults",
     }),
---
+++ src/functions/pluginsRemark.ts
@@ -0,0 +1,94 @@
+import { visit } from "unist-util-visit";
+import uniq from "lodash/uniq";
+import { h } from "hastscript";
+import { toHast } from "mdast-util-to-hast";
+
+export function ottieniDescrizione() {
+  return function (tree: any, { data }: { data: any }) {
+    let testoDaRitornare = "";
+    visit(tree, "paragraph", (paragrafi) => {
+    
+      
+      for (const figlio of paragrafi.children) {
+        if (testoDaRitornare.length > 150) return;
+        if (figlio.type === "text") testoDaRitornare += figlio.value;
+      }
+    });
+    data.astro.frontmatter.descrizione = testoDaRitornare;
+  };
+}
+
+export function ottieniSimboli() {
+  return function (tree: any, { data }: { data: any }) {
+    const matematica: string[] = [];
+    visit(tree, "inlineMath", (paragrafi) => {
+      matematica.push(paragrafi.value);
+    });
+    data.astro.frontmatter.formule = uniq(matematica);
+  };
+}
+
+export function boxDefinizione() {
+  return (tree: any) => {
+    visit(tree, "containerDirective", (node) => {
+      switch (node.name) {
+        case "def":
+          ritornaBox(
+            "definizione",
+            "https://cdn.betterttv.net/emote/5d63e543375afb1da9a68a5a/2x",
+            node
+          );
+          break;
+
+        case "att":
+          ritornaBox(
+            "attenzione",
+            "https://cdn.betterttv.net/emote/56e9f494fff3cc5c35e5287e/2x",
+            node
+          );
+          break;
+
+        case "nb":
+          ritornaBox(
+            "notaBene",
+            "https://cdn.betterttv.net/emote/5e2914861df9195f1a4cd411/2x",
+            node,
+            "Nota Bene"
+          );
+          break;
+      }
+    });
+  };
+}
+
+function ritornaBox(nome: string, urlImmagine: string, node: any, nomeDaVisualizzare?: string) {
+  const data = node.data || (node.data = {});
+  const tagName = "div";
+  data.hName = tagName;
+  const containerDefinizione = h(`.contenuto${nome}`, []);
+  for (const child of node.children) {
+    containerDefinizione.children.push(toHast(child) as any);
+  }
+
+  data.hProperties = h(`div.${nome}Container`, { class: ["conteinerSpegazioni"] }).properties;
+  data.hChildren = h(`div.${nome}Wraper`, [
+    h(`.boxIntestazione${nome}`, { class: ["intestazioneSpiegazione"] }, [
+      h("span", nomeDaVisualizzare ?? nome),
+      h(`img.immagineIntestazione${nome}`, {
+        src: urlImmagine,
+      }),
+    ]),
+    containerDefinizione,
+  ]).children;
+}
+
+
+export function codiceInline() {
+  return (tree: any) => {
+    visit(tree, "inlineCode", (node) => {
+      const data = node.data || (node.data = {});
+      data.hProperties = h(".codeInline").properties;
+      console.log(node);
+    });
+  };
+}
