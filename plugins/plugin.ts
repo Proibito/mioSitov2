16721286e9b682af5c1d5a0635d2cbc9d298bd9f
Proibito04 <30660830+Proibito@users.noreply.github.com>
2022-10-02 20:20:28 +0200

    aggiornamento vista non bruciata

--- plugins/plugin.ts
+++ plugins/plugin.ts
@@ -1,15 +1,26 @@
-import type { AstroIntegration } from "astro"; 
-import {creaDizionario} from "./plugins/scriptGeneraDizionario.ts"
+import type { AstroIntegration } from "astro";
+import { creaDizionario } from "./plugins/scriptGeneraDizionario.ts";
 
-export function miaEstensione(){
-    return{
-        name: "mia utility",
-        hooks: {
-            'astro:build:start': (opzioni) => {
-                console.log("ciao");
-                
-            }
-
-        }
-    } as AstroIntegration
+export function miaEstensione() {
+  return {
+    name: "mia utility",
+    hooks: {
+      "astro:config:setup": (opzioni) => {
+        opzioni.injectScript(
+          "head-inline",
+          `if (localStorage.getItem("temaNotte") === null) {
+                    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+                      document.getElementsByTagName("html")[0].classList.add("night");
+                    }
+                  } else {
+                    if (localStorage.getItem("temaNotte") == "true")
+                      document.getElementsByTagName("html")[0].classList.add("night");
+                  }`
+        );
+      },
+      "astro:build:start": (opzioni) => {
+        console.log("ciao");
+      },
+    },
+  } as AstroIntegration;
 }
